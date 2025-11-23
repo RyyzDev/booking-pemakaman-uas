aa62a5be4455b25248f75614de3bea9a4aba7216
RyyzDev <fachriakbarkutubi@gmail.com>
2025-11-24 04:27:14 +0700

    fix2 api

--- resources/js/config/axios.js
+++ resources/js/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //axios.defaults.withCredentials = true; 
-axios.defaults.baseURL = 'https://pemakaman.alwaysdata.net/api'; 
+axios.defaults.baseURL = 'https://pemakaman.alwaysdata.net'; 
 const token = localStorage.getItem('authToken');
 if (token) {
     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
